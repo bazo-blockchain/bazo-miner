15a84e4a1a59d92c9eb233a5af0b1bbc378e0038
mchetelat <m.chetelat@me.com>
2018-02-13 11:02:52 +0100

    Rename config constants

--- storage/configs.go
+++ storage/configs.go
@@ -3,9 +3,9 @@ package storage
 const (
 	//Root Public Keys at initialization time. This is the only existing account at startup
 	//All other accounts are created
-	INITROOTKEY1 = "f894ba7a24c1c324bc4b0a833d4b076a0e0f675a380fb7e782672c6568aaab06"
-	INITROOTKEY2 = "69ddbc62f79cb521411840d83ff0abf941a8e717d81af3dfc2973f1bac30308a"
-	INITPRIVKEY = "4e8cf4d82d1a376484659b632c0c506affe3c394d18266379c1c6b86eb5ba0fb"
+	INITROOTPUBKEY1 = "f894ba7a24c1c324bc4b0a833d4b076a0e0f675a380fb7e782672c6568aaab06"
+	INITROOTPUBKEY2 = "69ddbc62f79cb521411840d83ff0abf941a8e717d81af3dfc2973f1bac30308a"
+	INITROOTPRIVKEY = "4e8cf4d82d1a376484659b632c0c506affe3c394d18266379c1c6b86eb5ba0fb"
 
 	//Sha3-256 Hash of the MINER'S ECC public key (needs to be part of the state)
 	BENEFICIARY = "1c90d27e539d035512d27d072f7b514753157fa1591ff5c5a8a9ef642449d291"
