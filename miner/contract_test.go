a61200624cdda9ebac2490935b6c731005cf3c1c
marcosteiner <22612697+marcosteiner@users.noreply.github.com>
2018-06-07 12:56:11 +0200

    Refactored Tokenization Contract

--- miner/contract_test.go
+++ miner/contract_test.go
@@ -152,9 +152,8 @@ func TestMultipleBlocksWithTokenizationContractTx(t *testing.T) {
 
 	b := newBlock([32]byte{}, [32]byte{}, [32]byte{}, 1)
 	contract := []byte{
-		// 35, 1, 0, 0, 1, 10, 22, 0, 11, 3, 49, 28, 0, 28, 1, 29, 0, 0, 33, 10, 22, 0, 25, 2, 24, 28, 0, 29, 0, 2, 38, 28, 1, 4, 39, 27, 0, 2, 24,
-		// 35, 1, 0, 0, 1, 10, 22, 0, 11, 2, 50, 28, 0, 28, 1, 29, 1, 33, 10, 22, 0, 24, 2, 24, 28, 0, 29, 2, 39, 4, 28, 1, 29, 2, 40, 27, 2, 24,
-		35, 1, 0, 0, 1, 10, 22, 0, 11, 3, 50, 28, 0, 28, 1, 29, 1, 33, 10, 22, 0, 24, 2, 24, 28, 1, 29, 2, 39, 28, 0, 4, 28, 1, 29, 2, 40, 27, 2, 24,
+		//35, 1, 0, 0, 1, 10, 22, 0, 11, 3, 50, 28, 1, 28, 0, 29, 1, 33, 10, 22, 0, 24, 2, 24, 28, 1, 28, 0, 1, 29, 2, 37, 22, 0, 46, 2, 28, 1, 28, 0, 29, 2, 38, 27, 2, 50, 28, 1, 29, 2, 39, 28, 1, 4, 29, 2, 40, 27, 2, 24
+		35, 1, 0, 0, 1, 10, 22, 0, 11, 3, 50, 28, 1, 28, 0, 29, 1, 33, 10, 22, 0, 24, 2, 24, 28, 1, 28, 0, 1, 29, 2, 37, 22, 0, 46, 2, 28, 1, 28, 0, 29, 2, 38, 27, 2, 50, 28, 1, 29, 2, 39, 28, 0, 4, 28, 1, 29, 2, 40, 27, 2, 50,
 	}
 
 	contractVariables := make([]protocol.ByteArray, 3)
@@ -198,6 +197,61 @@ func TestMultipleBlocksWithTokenizationContractTx(t *testing.T) {
 
 	actual := uint64(tmp[1])
 	expected := uint64(101)
+	if expected != actual {
+		t.Errorf("State change not persisted, expected: '%v', but is: '%v'", expected, actual)
+	}
+}
+
+func TestMultipleBlocksWithTokenizationContractTxWhichAddsKey(t *testing.T) {
+	cleanAndPrepare()
+
+	b := newBlock([32]byte{}, [32]byte{}, [32]byte{}, 1)
+	contract := []byte{
+		//35, 1, 0, 0, 1, 10, 22, 0, 11, 3, 50, 28, 0, 28, 1, 29, 1, 33, 10, 22, 0, 24, 2, 24, 28, 0, 29, 2, 37, 22, 0, 45, 2, 28, 0, 28, 1, 29, 2, 38, 27, 2, 50, 28, 1, 29, 2, 39, 28, 0, 4, 29, 2, 40, 27, 2, 24,
+		35, 1, 0, 0, 1, 10, 22, 0, 11, 3, 50, 28, 1, 28, 0, 29, 1, 33, 10, 22, 0, 24, 2, 24, 28, 1, 28, 0, 1, 29, 2, 37, 22, 0, 46, 2, 28, 1, 28, 0, 29, 2, 38, 27, 2, 50, 28, 1, 29, 2, 39, 28, 0, 4, 28, 1, 29, 2, 40, 27, 2, 50,
+	}
+
+	contractVariables := make([]protocol.ByteArray, 3)
+	receiver := []byte{0x00, 0x2b}
+	contractVariables[0] = receiver
+
+	minter := []byte{0x6e, 0x60, 0x66, 0x30, 0x48, 0x5f, 0xa2, 0xf5, 0xc6, 0x5c, 0x2f, 0x67, 0x97, 0x96, 0xd9, 0x2a, 0xcc, 0x27, 0x19, 0x0d, 0x63, 0x3a, 0x2d, 0xd7, 0x07, 0x40, 0x2e, 0x47, 0x93, 0xf3, 0xf2, 0xa2}
+	contractVariables[1] = minter
+
+	m := vm.NewMap()
+	//m.Append(receiver, []byte{0x00, 0x01})
+	contractVariables[2] = []byte(m)
+
+	createBlockWithSingleContractDeployTx(b, contract, contractVariables)
+	finalizeBlock(b)
+	if err := validateBlock(b); err != nil {
+		t.Errorf("Block validation for (%v) failed: %v\n", b, err)
+	}
+
+	b1 := newBlock(b.Hash, [32]byte{}, [32]byte{}, 2)
+	transactionData := []byte{
+		1, 0, 100, // Amount
+		1, receiver[0], receiver[1], // receiver address
+		1, 0, 1, // function Hash
+	}
+	hash := createBlockWithSingleContractCallTx(b1, transactionData)
+	finalizeBlock(b1)
+	if err := validateBlock(b1); err != nil {
+		t.Errorf("Block validation failed: %v\n", err)
+	}
+
+	m, err := vm.MapFromByteArray(storage.GetAccount(hash).ContractVariables[2])
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	tmp, err := m.GetVal(receiver)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	actual := uint64(tmp[1])
+	expected := uint64(100)
 	if expected != actual {
 		t.Errorf("State change not persisted, expected: '%v', but is: '%v'", expected, actual)
 	}
