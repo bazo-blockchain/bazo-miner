b595df471b3cc7c60ed37e6e5abd3d7182f93bad
Marc-Alain Chételat <m.chetelat@me.com>
2018-06-14 08:55:18 +0200

    Remove unused imports

--- miner/blockchain.go
+++ miner/blockchain.go
@@ -4,12 +4,10 @@ import (
 	"crypto/ecdsa"
 	"errors"
 	"fmt"
-	"github.com/bazo-blockchain/bazo-miner/p2p"
 	"github.com/bazo-blockchain/bazo-miner/protocol"
 	"github.com/bazo-blockchain/bazo-miner/storage"
 	"log"
 	"sync"
-	"time"
 )
 
 var (
