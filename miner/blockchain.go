8dce46dd99ed9b8a963e3f85a6fb69319bc69dab
Marc-Alain Chételat <m.chetelat@me.com>
2018-06-28 09:58:45 +0200

    Bugfix

--- miner/blockchain.go
+++ miner/blockchain.go
@@ -110,7 +110,7 @@ func initRootKey() ([32]byte, error) {
 	}
 
 	//Balance must be greater than the staking minimum.
-	rootAcc := protocol.NewAccount(address, activeParameters.Staking_minimum+1, true, hashedSeed)
+	rootAcc := protocol.NewAccount(address, activeParameters.Staking_minimum, true, hashedSeed)
 	storage.State[addressHash] = &rootAcc
 	storage.RootKeys[addressHash] = &rootAcc
 
