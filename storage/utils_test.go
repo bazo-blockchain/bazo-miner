80e2191d344dd0540c8e54f0e3e3d12e0aa6bb8d
Marc-Alain Chételat <m.chetelat@me.com>
2018-09-16 14:48:35 +0200

    Add tests

---
+++ storage/utils_test.go
@@ -0,0 +1,58 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/bazo-blockchain/bazo-miner/protocol"
+	"math/big"
+	"testing"
+)
+
+func TestSerializeHashContent(t *testing.T) {
+	var data []byte
+	pubKeyInt, _ := new(big.Int).SetString(PubA1+PubA2, 16)
+	copy(data, pubKeyInt.Bytes())
+
+	hash := SerializeHashContent(data)
+
+	if fmt.Sprintf("%x", hash) != "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a" {
+		t.Errorf("Error serializing: %x != %v\n", hash, "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a")
+	}
+}
+
+func TestGetAccount(t *testing.T) {
+	accAHash := protocol.SerializeHashContent(accA.Address)
+
+	acc, err := GetAccount(accAHash)
+
+	if acc != accA && err == nil {
+		t.Errorf("Error fetching account from state: %x\n", accAHash)
+	}
+
+	if acc == accB && err == nil {
+		t.Errorf("Error fetching account from state: %x\n", accAHash)
+	}
+
+	var nilHash [32]byte
+	acc, err = GetAccount(nilHash)
+
+	if acc != nil || err.Error() != fmt.Sprintf("Acc (%x) not in the state.", nilHash[0:8]) {
+		t.Errorf("Error fetching account from state: %x\n", nilHash)
+	}
+}
+
+func TestGetRootAccount(t *testing.T) {
+	rootHash := protocol.SerializeHashContent(rootAcc.Address)
+
+	root, err := GetRootAccount(rootHash)
+
+	if root == nil || err != nil {
+		t.Errorf("Error fetching root account from state: %x\n", rootHash)
+	}
+
+	var nilHash [32]byte
+	root, err = GetRootAccount(nilHash)
+
+	if root != nil {
+		t.Errorf("Error fetching account from state: %x\n", nilHash)
+	}
+}
