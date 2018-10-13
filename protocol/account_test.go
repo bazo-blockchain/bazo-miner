99aad36d64c77079e4bad25e01be59058b9678fa
Marc-Alain Chételat <m.chetelat@me.com>
2018-10-13 17:29:58 +0200

    Revise test

--- protocol/account_test.go
+++ protocol/account_test.go
@@ -6,11 +6,18 @@ import (
 )
 
 func TestAccountSerialization(t *testing.T) {
+	addTestingAccounts()
+
+	accA.Balance = 1000
+	accA.IsStaking = true
+	accA.TxCnt = 5
+	accA.StakingBlockHeight = 100
+
 	var compareAcc *Account
 	encodedAcc := accA.Encode()
 	compareAcc = compareAcc.Decode(encodedAcc)
 
 	if !reflect.DeepEqual(accA, compareAcc) {
-		t.Error("Account Serialization failed!")
+		t.Error("Account encoding/decoding failed!")
 	}
 }
