335c0f06a3c1c05c5c2bb439cd8ced594f9871c1
Roman Blum <rmnblm@icloud.com>
2018-11-20 10:35:36 +0100

    partly cherry-pick 727e2360de6f8cd75aad62b94f097379b181f2f9 :cherries:

--- storage/main_test.go
+++ storage/main_test.go
@@ -3,6 +3,8 @@ package storage
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"crypto/rsa"
+	"github.com/bazo-blockchain/bazo-miner/crypto"
 	"github.com/bazo-blockchain/bazo-miner/protocol"
 	"io/ioutil"
 	"log"
@@ -20,6 +22,10 @@ const (
 	PubA1 = "c2be9abbeaec39a066c2a09cee23bb9ab2a0b88f2880b1e785b4d317adf0dc7c"
 	PubA2 = "8ce020fde838d9c443f6c93345dafe7fd74f091c4d2f30b37e2453679a257ed5"
 	PrivA = "ba127fa8f802b008b9cdb58f4e44809d48f1b000cff750dda9cd6b312395c1c5"
+	CommPubA = "vsl0yfAd3dqJfDEawAl7Xp2/hOvXGN/u0UXBpRSxWAT+FSKlt5Ha8Ibd59tGkM4D8i/MABx0MMNEVL8Ghe1QkXIITJRnFtoqsidTlcHSL4WL7sc+8LwJjIjMdqM5BYIZJap/j2O2qREcEICEN8i+6LF844iMqFysDOuL8F5MAH22twrh0SMVXAM+IAEqa0Z9TymvX8Op3dt/t5IhrA4ivsS/+QMWzr3xJE9XfQxMrDUNoBwXIszOr656m8/wYa9dOEZn8qlglEySAjievkECJZq9Q3DRat5SUoXjG8M6UJp/AeRUUANsXhrPn6Cg7j4ke5Yw0bk6Lz9foYaZ9rugkw=="
+	CommPrivA = "n5Xdlei+4sshA3wDlyyXQF6NS78GTi1KE0zZHJ/BdBHBAqbXnURosZbuWTmmvgtFa7ilWFZ0rjE3n/elmjMWmIKdBImB7bCR1DFnDjZw/QUlNpb9Q9rV1fK7rGT9lmjrZgFG8AcFTEgehIMrlYnafsOv5pdaqJ3T4H7KsEYAJsuNZhHAFmReqNdeiUbdAntPLQjttbs43DqaVQ0D3YnHrKxeu7Ekwcs4ap18tkFt7Lp0mkJ3fjpsvJFPDP2CotrZadLilv7dmOrXe26XDLUQ2aBguExV4Wx85J29puOJwpoM60KiFgiBMtRQFRukzRuValiVkXEBLZKlbh6wYy0vwQ=="
+	CommPrim1A = "9UbkVH5chUZCZaehntnZWAfTJ9OYvsKfu19Cb39RrBZ9FDMjDoBlKZslyvRzTez33An84JAgwOBtEbaSTAkVqvPmDin3oZhTYbwwDc9SIBVsYhI6VmbjcPkMAFIeoKbS4KzweXneeKBB9FbozcgvnYrv3lTqofVVWONY/EL9q7M="
+	CommPrim2A = "xyC4Jl6ojvL+uF2/iK9kRj3yQh8bV2ngl/fongysmUvxCZrwxaEaOZcBHreTiP6SFPOrWCyk6e9zHjtDPP/LhxrHsaiFapv6AjQejML/gCyFj4GRWMzayFBJlW6prsjZfhNG6FpQbFrEj8FtYdM0vRLyDyzeknrC66PJtwEcR6E="
 	PubB1 = "5d7eefd58e3d2f309471928ab4bbd104e52973372c159fa652b8ca6b57ff68b8"
 	PubB2 = "ab301a6a77b201c416ddc13a2d33fdf200a5302f6f687e0ea09085debaf8a1d9"
 	PrivB = "7a0a9babcc97ea7991ed67ed7f800f70c5e04e99718960ad8efab2ca052f00c7"
@@ -32,10 +38,13 @@ const (
 	RootPriv = "277ed539f56122c25a6fc115d07d632b47e71416c9aebf1beb54ee704f11842c"
 )
 
-var accA, accB, minerAcc, rootAcc *protocol.Account
-var PrivKeyA, PrivKeyB ecdsa.PrivateKey
-var PubKeyA, PubKeyB ecdsa.PublicKey
-var RootPrivKey ecdsa.PrivateKey
+var (
+	accA, accB, minerAcc, rootAcc *protocol.Account
+	PrivKeyA, PrivKeyB ecdsa.PrivateKey
+	PubKeyA, PubKeyB ecdsa.PublicKey
+	CommitmentKeyA 	*rsa.PrivateKey
+	RootPrivKey ecdsa.PrivateKey
+)
 
 func TestMain(m *testing.M) {
 
@@ -67,6 +76,8 @@ func addTestingAccounts() {
 		PubKeyA,
 		priva,
 	}
+
+	CommitmentKeyA, _ = crypto.CreateRSAPrivKeyFromBase64(CommPubA, CommPrivA, []string{CommPrim1A, CommPrim2A})
 
 	pubb1, _ := new(big.Int).SetString(PubB1, 16)
 	pubb2, _ := new(big.Int).SetString(PubB2, 16)
