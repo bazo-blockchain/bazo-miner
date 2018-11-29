c1bc30b2d8e0241b9bc64db39e9ad6aa0e5f604c
febe19 <fabio.maddaloni@uzh.ch>
2018-11-29 17:03:32 +0100

    new block size and intervall

--- miner/configs.go
+++ miner/configs.go
@@ -16,9 +16,9 @@ const (
 	//TODO @simibac How can I assure that only every min. a block will be mined?
 	BLOCKHASH_SIZE       = 32      //Byte
 	FEE_MINIMUM          = 1       //Coins
-	BLOCK_SIZE           = 5000000 //Byte
+	BLOCK_SIZE           = 1000000 //Byte
 	DIFF_INTERVAL        = 50    //Blocks
-	BLOCK_INTERVAL       = 60      //Sec
+	BLOCK_INTERVAL       = 180      //Sec
 	BLOCK_REWARD         = 0       //Coins
 	STAKING_MINIMUM      = 1000    //Coins
 	WAITING_MINIMUM      = 0       //Blocks
