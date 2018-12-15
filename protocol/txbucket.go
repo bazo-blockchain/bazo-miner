ce6457e1bf720bee79426b43d8e1457d2e4752b3
Roman Blum <rmnblm@icloud.com>
2018-12-15 09:59:53 +0100

    improve TxBucket and add tests

--- protocol/txbucket.go
+++ protocol/txbucket.go
@@ -4,14 +4,16 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"log"
+	"sort"
 )
 
 type TxBucket struct {
-	Hash		[32]byte
-	Address		[64]byte
-	Amount		uint64
-	MerkleRoot	[32]byte
-	FundsTxData [][32]byte
+	Address         [64]byte
+	RelativeBalance int64
+
+	merkleRoot hashType
+	txHashes   hashArray
 }
 
 func NewTxBucket(address [64]byte) *TxBucket {
@@ -20,21 +22,58 @@ func NewTxBucket(address [64]byte) *TxBucket {
 	}
 }
 
-func (bucket *TxBucket) CreateHash() [32]byte {
+func (bucket *TxBucket) AddFundsTx(tx *FundsTx) {
+	if tx.From == bucket.Address {
+		// Bucket owner is sender of the transaction, must pay the amount and the fee
+		bucket.RelativeBalance -= int64(tx.Amount + tx.Fee)
+	} else if tx.To == bucket.Address {
+		// Bucket owner is receiver of the transaction, must not pay for the fee
+		bucket.RelativeBalance += int64(tx.Amount)
+	} else {
+		return
+	}
+
+	hash := tx.Hash()
+	bucket.txHashes = append(bucket.txHashes, hash)
+}
+
+func (bucket *TxBucket) CalculateMerkleRoot() hashType {
+	emptyHash := hashType{}
+	if bucket.merkleRoot == emptyHash {
+		merkleTree := bucket.buildMerkleTree()
+		bucket.merkleRoot = merkleTree.MerkleRoot()
+	}
+	return bucket.merkleRoot
+}
+
+func (bucket *TxBucket) buildMerkleTree() *MerkleTree {
 	if bucket == nil {
-		return [32]byte{}
+		return nil
+	}
+
+	//Merkle root for no transactions is 0 hash
+	if len(bucket.txHashes) == 0 {
+		return nil
+	}
+
+	m, _ := NewMerkleTree(bucket.txHashes)
+
+	return m
+}
+
+func (bucket *TxBucket) Hash() hashType {
+	if bucket == nil {
+		return hashType{}
 	}
 
 	bucketHash := struct {
-		hash       [32]byte
 		address    [64]byte
-		amount		uint64
-		merkleRoot [32]byte
+		amount	   int64
+		merkleRoot hashType
 	}{
-		bucket.Hash,
 		bucket.Address,
-		bucket.Amount,
-		bucket.MerkleRoot,
+		bucket.RelativeBalance,
+		bucket.CalculateMerkleRoot(),
 	}
 
 	return SerializeHashContent(bucketHash)
@@ -46,11 +85,9 @@ func (bucket *TxBucket) Encode() []byte {
 	}
 
 	encoded := TxBucket{
-		Hash:			bucket.Hash,
-		Address:		bucket.Address,
-		MerkleRoot:     bucket.MerkleRoot,
-		Amount:			bucket.Amount,
-		FundsTxData:	bucket.FundsTxData,
+		Address:         bucket.Address,
+		RelativeBalance: bucket.RelativeBalance,
+		merkleRoot:      bucket.CalculateMerkleRoot(),
 	}
 
 	buffer := new(bytes.Buffer)
@@ -71,13 +108,48 @@ func (bucket *TxBucket) Decode(encoded []byte) (b *TxBucket) {
 }
 
 func (bucket TxBucket) String() string {
+	hash := bucket.Hash()
+	merkleRoot := bucket.CalculateMerkleRoot()
+
 	return fmt.Sprintf("\nHash: %x\n" +
 		"Address: %x\n" +
-		"Amount: %v\n"+
-		"MerkleRoot: %x\n",
-		bucket.Hash[0:8],
+		"Relative Balance: %v\n"+
+		"Merkle Root: %x\n",
+		hash[0:8],
 		bucket.Address[0:8],
-		bucket.Amount,
-		bucket.MerkleRoot[0:8],
+		bucket.RelativeBalance,
+		merkleRoot[0:8],
 	)
+}
+
+type txBucketMap map[addressType]*TxBucket
+
+func NewTxBucketMap() txBucketMap {
+	return make(txBucketMap)
+}
+
+func (m txBucketMap) Sort() txBucketMap {
+	var buckets []*TxBucket
+	for _, bucket := range m {
+		buckets = append(buckets, bucket)
+	}
+
+	sort.Slice(buckets, func(i, j int) bool {
+		switch bytes.Compare(buckets[i].Address[:], buckets[j].Address[:]) {
+		case -1:
+			return true
+		case 0, 1:
+			return false
+		default:
+			log.Panic("not fail-able with `bytes.Comparable` bounded [-1, 1].")
+			return false
+		}
+	})
+
+	sortedMap := NewTxBucketMap()
+	for _, bucket := range buckets {
+		sortedMap[bucket.Address] = bucket
+	}
+
+	return sortedMap
 }
--- protocol/txbucket_test.go
+++ protocol/txbucket_test.go
@@ -12,7 +12,7 @@ func TestBucketCreation(t *testing.T) {
 	bucket := NewTxBucket(address)
 
 	if !reflect.DeepEqual(bucket.Address, address) {
-		t.Errorf("Address does not match the given one: %x vs. %x", bucket.Address, address)
+		t.Errorf("BucketAddress does not match the given one: %x vs. %x", bucket.Address, address)
 	}
 }
 
@@ -21,22 +21,22 @@ func TestBucketHash(t *testing.T) {
 	rand.Read(address[:])
 	bucket := NewTxBucket(address)
 
-	hash1 := bucket.CreateHash()
+	hash1 := bucket.Hash()
 
-	if !reflect.DeepEqual(hash1, bucket.CreateHash()) {
+	if !reflect.DeepEqual(hash1, bucket.Hash()) {
 		t.Errorf("Bucket hashing failed!")
 	}
 
 	rand.Read(address[:])
 	bucket.Address = address
 
-	hash2 := bucket.CreateHash()
+	hash2 := bucket.Hash()
 
 	if reflect.DeepEqual(hash1, hash2) {
 		t.Errorf("Bucket hashing failed!")
 	}
 
-	if !reflect.DeepEqual(hash2, bucket.CreateHash()) {
+	if !reflect.DeepEqual(hash2, bucket.Hash()) {
 		t.Errorf("Bucket hashing failed!")
 	}
 }
@@ -44,10 +44,8 @@ func TestBucketHash(t *testing.T) {
 func TestBucketSerialization(t *testing.T) {
 	var bucket TxBucket
 
-	rand.Read(bucket.Hash[:])
 	rand.Read(bucket.Address[:])
-	bucket.Amount = rand.Uint64()
-	rand.Read(bucket.MerkleRoot[:])
+	bucket.RelativeBalance = rand.Int63()
 
 	encodedBucket := bucket.Encode()
 
@@ -55,5 +53,41 @@ func TestBucketSerialization(t *testing.T) {
 	compareBucket = *compareBucket.Decode(encodedBucket)
 	if !reflect.DeepEqual(bucket, compareBucket) {
 		t.Error("Bucket encoding/decoding failed!")
+	}
+}
+
+func TestBucketAddFundsTx(t *testing.T) {
+	bucket := NewTxBucket(accA.Address)
+
+	amount := uint64(100)
+	fee := uint64(1)
+
+	tx := NewSimpleFundsTx(amount, fee, uint32(0), accA.Address, accB.Address)
+	bucket.AddFundsTx(tx)
+
+	expectedAmount := int64(amount + fee) * -1
+	if bucket.RelativeBalance != expectedAmount {
+		t.Errorf("invalid bucket amount, is %v but should be %v", bucket.RelativeBalance, expectedAmount)
+	}
+
+	amount = uint64(300)
+
+	tx = NewSimpleFundsTx(amount, fee, uint32(0), accB.Address, accA.Address)
+	bucket.AddFundsTx(tx)
+
+	expectedAmount = expectedAmount + int64(amount)
+	if bucket.RelativeBalance != expectedAmount {
+		t.Errorf("invalid bucket amount, is %v but should be %v", bucket.RelativeBalance, expectedAmount)
+	}
+
+	var randomAddressA, randomAddressB [64]byte
+	rand.Read(randomAddressA[:])
+	rand.Read(randomAddressB[:])
+
+	tx = NewSimpleFundsTx(amount, fee, uint32(0), randomAddressA, randomAddressB)
+	bucket.AddFundsTx(tx)
+
+	if bucket.RelativeBalance != expectedAmount {
+		t.Errorf("invalid bucket amount, is %v but should be %v", bucket.RelativeBalance, expectedAmount)
 	}
 }
